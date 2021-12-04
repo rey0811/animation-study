6482514b43c98065a0a2f3d130c2ebc93cd7b18c
rey0811 <reotogashi@gmail.com>
2021-12-04 11:56:32 +0900

    Initial commit

---
+++ from-bg-to-element/index.js
@@ -0,0 +1,23 @@
+// animation関数
+function scrollAnimation() {
+    $(window).scroll(function () {
+      $(".js-anim").each(function () {
+        var elemPos = $(this).offset().top-0;//要素より、0px上の
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight){
+          $(this).addClass('is-animated');// 画面内に入ったらis-animatedというクラス名を追記
+        }
+        });
+    });
+};
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+    scrollAnimation();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+    scrollAnimation();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
